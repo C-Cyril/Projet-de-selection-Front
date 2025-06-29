ce6b76d8f11704272a57d2b30637fe3fac0e14c5
C-Cyril <pro.catteau@gmail.com>
2025-06-29 21:47:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function blagueAleatoire() {
+  const apiURL = "https://projet-de-selection-back.onrender.com/blagues/random"
+  const questionHTML = document.getElementById("question");
+  const reponseHTML = document.getElementById("reponse");
+  
+  questionHTML.textContent = "test Q";
+  reponseHTML.textContent = "test R";
+  
+  fetch (apiURL).then(reponseApi => {
+    if (!reponseApi.ok) {
+      throw new Error('Erreur lors de la demande de blague');
+    }
+    alert('Reussite');
+    return reponseApi;
+  })
+  .then(blague => {
+    questionHTML.textContent = "test question";
+    reponseHTML.textContent = "test réponse";
+  })
+  .catch(error => {
+    console.error('Error: ', error);
+  });
+  
+};
+
