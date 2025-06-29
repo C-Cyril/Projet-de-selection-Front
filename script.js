d8cabb4af52e7f3dd0e038c24c51bb96fe5d124b
C-Cyril <pro.catteau@gmail.com>
2025-06-29 19:14:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const apiURL = "https://projet-de-selection-back.onrender.com/blagues/random"
+const questionHTML = document.getelementByClassName("question");
+const questionHTML = document.getelementByClassName("reponse");
 
+function blagueAleatoire () {
+  fetch (apiURL).then(reponseApi => {
+    if (!reponseApi.ok) {
+      throw new Error('Erreur lors de la demande de blague');
+    }
+    return reponseApi;
+  })
+  .then(blague => {
+    questionHTML.textContent = "test question";
+    reponseHTML.textContent = "test réponse";
+  })
+  .catch(error => {
+    console.error('Error: ', error);
+  });
+}
