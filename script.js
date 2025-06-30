390ea1659eb522adb6a6a5fc97c1a8121b66537d
C-Cyril <pro.catteau@gmail.com>
2025-06-30 15:37:26 +0200

    WIP

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ function blagueAleatoire() {
   const apiURL = "https://projet-de-selection-back.onrender.com/blagues/random"
   const questionHTML = document.getElementById("question");
   const reponseHTML = document.getElementById("reponse");
-  
-  questionHTML.textContent = "test Ques";
-  reponseHTML.textContent = "test Rep";
-  
+
   fetch (apiURL).then(reponseApi => {
     if (!reponseApi.ok) {
       throw new Error('Erreur lors de la demande de blague');
@@ -20,6 +17,4 @@ function blagueAleatoire() {
   .catch(error => {
     console.error('Error: ', error);
   });
-  
 };
-
